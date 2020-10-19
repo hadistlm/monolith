1232c87f815d5caba67512f2fb06d06717b1c26a
Ovensif <imamtaufikt.ti.poliwangi@gmail.com>
2020-10-19 16:39:42 +0700

    Running monolith_backend with postgres server, add new file api for testing api's next

---
+++ app/Controllers/Http/api/TestingController.js
@@ -0,0 +1,88 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with testings
+ */
+class TestingController {
+    /**
+     * Show a list of all testings.
+     * GET testings
+     *
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     * @param {View} ctx.view
+     */
+    async index({ request, response, view }) {
+
+    }
+
+    /**
+     * Render a form to be used for creating a new testing.
+     * GET testings/create
+     *
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     * @param {View} ctx.view
+     */
+    async create({ request, response, view }) {}
+
+    /**
+     * Create/save a new testing.
+     * POST testings
+     *
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     */
+    async store({ request, response }) {}
+
+    /**
+     * Display a single testing.
+     * GET testings/:id
+     *
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     * @param {View} ctx.view
+     */
+    async show({ params, request, response, view }) {}
+
+    /**
+     * Render a form to update an existing testing.
+     * GET testings/:id/edit
+     *
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     * @param {View} ctx.view
+     */
+    async edit({ params, request, response, view }) {}
+
+    /**
+     * Update testing details.
+     * PUT or PATCH testings/:id
+     *
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     */
+    async update({ params, request, response }) {}
+
+    /**
+     * Delete a testing with id.
+     * DELETE testings/:id
+     *
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     */
+    async destroy({ params, request, response }) {}
+}
+
+module.exports = TestingController
